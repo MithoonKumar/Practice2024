231f44e9e7aa060a7823b414e9eb5f3c37cf2a6b
Mithoon Kumar <mithoonkumar@Mithoons-MacBook-Air.local>
2024-10-06 13:29:14 +0530

    Adds the usage of hashmap

---
+++ src/main/java/org/example/UsageOfHashMap.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageOfHashMap {
+
+    public static void main(String[] args) {
+        System.out.println("Usage of hashmap");
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("first", 1);
+        hashMap.put("second", 2);
+        hashMap.put("third", 3);
+
+        System.out.println(hashMap.get("first"));
+
+        for (Map.Entry<String, Integer> entry: hashMap.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+}
