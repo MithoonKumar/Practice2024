7472671d32957ba310bca1e35012ee70cb67d89a
Mithoon Kumar <mithoonkumar@Mithoons-MacBook-Air.local>
2024-12-05 16:37:07 +0530

    Adds code for sliding window rate limiter

---
+++ src/main/java/Atlassian/FixedWindowRateLimiter.java
@@ -0,0 +1,52 @@
+package Atlassian;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class FixedWindowRateLimiter {
+    private final long windowLengthInMillis;
+    private long windowStartTime;
+    private long windowEndTime;
+    private final long maxRequestsAllowed;
+    private long currentWindowRequestCount;
+
+
+    public FixedWindowRateLimiter(long windowLengthInMillis, long maxRequestsAllowed) {
+        if (windowLengthInMillis <= 0) {
+            throw new IllegalArgumentException("Window length must be positive");
+        }
+        if (maxRequestsAllowed <=0 ){
+            throw new IllegalArgumentException("Number of requests allowed must be positive");
+        }
+        this.windowLengthInMillis = windowLengthInMillis;
+        this.maxRequestsAllowed = maxRequestsAllowed;
+        this.windowStartTime = System.currentTimeMillis();
+        this.windowEndTime = this.windowStartTime + this.windowLengthInMillis;
+        this.currentWindowRequestCount = 0;
+    }
+
+
+    public synchronized boolean isAllowed() {
+        long currentTimeStamp = System.currentTimeMillis();
+        if (currentTimeStamp >= this.windowEndTime) {
+            this.windowStartTime = currentTimeStamp;
+            this.windowEndTime = this.windowStartTime + this.windowLengthInMillis;
+            this.currentWindowRequestCount = 1;
+            return true;
+        } else if (this.currentWindowRequestCount < this.maxRequestsAllowed) {
+            this.currentWindowRequestCount++;
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        FixedWindowRateLimiter fixedWindowRateLimiter = new FixedWindowRateLimiter(1000L, 1L);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleAtFixedRate(()->{
+            System.out.println(fixedWindowRateLimiter.isAllowed());
+        }, 0, 100, TimeUnit.MILLISECONDS);
+    }
+}
---
+++ src/main/java/linkedin/Main.java
@@ -0,0 +1,8 @@
+package linkedin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
