d7c5d05bf4e5b4984d0ce0716fc08fd08032673f
Mithoon Kumar <mithoonkumar@Mithoons-MacBook-Air.local>
2024-12-05 16:56:59 +0530

    Adds code for sliding window rate limiter

---
+++ src/main/java/Atlassian/SlidingWindowRateLimiter.java
@@ -0,0 +1,40 @@
+package Atlassian;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SlidingWindowRateLimiter {
+    private final long windowLengthInMillis;
+    private final Queue<Long> queue;
+    private final long maxRequestsAllowed;
+
+
+    public SlidingWindowRateLimiter(long windowLengthInMillis, long maxRequestsAllowed) {
+        this.windowLengthInMillis = windowLengthInMillis;
+        this.maxRequestsAllowed = maxRequestsAllowed;
+        this.queue = new LinkedList<>();
+    }
+
+    public synchronized boolean isAllowed() {
+        long currentTime = System.currentTimeMillis();
+        while (!queue.isEmpty() && currentTime - queue.peek() > this.windowLengthInMillis) {
+            queue.poll();
+        }
+        if (this.queue.size() < this.maxRequestsAllowed) {
+            queue.add(currentTime);
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        SlidingWindowRateLimiter slidingWindowRateLimiter = new SlidingWindowRateLimiter(1000L, 1L);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleAtFixedRate(()->{
+            System.out.println(slidingWindowRateLimiter.isAllowed());
+        }, 0, 100, TimeUnit.MILLISECONDS);
+    }
+}
